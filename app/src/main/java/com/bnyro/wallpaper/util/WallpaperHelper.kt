d7c33ba16d631fd7bacde9c133bb4c460efe91a0
Emanuele Viglianisi <emavgl@gmail.com>
2023-12-24 12:33:42 +0100

    test3

--- app/src/main/java/com/bnyro/wallpaper/util/WallpaperHelper.kt
+++ app/src/main/java/com/bnyro/wallpaper/util/WallpaperHelper.kt
@@ -92,18 +92,24 @@ object WallpaperHelper {
         }.getOrDefault(resizedBitmap)
     }
 
-
     private fun getResizedBitmap(bitmap: Bitmap, newWidth: Int, newHeight: Int): Bitmap {
         val scaledBitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888)
         val ratioX = newWidth / bitmap.width
         val ratioY = newHeight / bitmap.height
 
+        val middleX = newWidth / 2.0f
+        val middleY = newHeight / 2.0f
+
         val scaleMatrix = Matrix()
-        scaleMatrix.setScale(ratioX.toFloat(), ratioY.toFloat(), 0F, 0F);
+        scaleMatrix.setScale(ratioX.toFloat(), ratioY.toFloat(), middleX, middleY)
 
         val canvas = Canvas(scaledBitmap)
         canvas.setMatrix(scaleMatrix);
-        canvas.drawBitmap(bitmap, 0F, 0F, Paint(Paint.FILTER_BITMAP_FLAG))
+        canvas.drawBitmap(bitmap,
+            middleX - bitmap.width / 2,
+            middleY - bitmap.height / 2,
+            Paint(Paint.FILTER_BITMAP_FLAG));
+
         return scaledBitmap
     }
 }
