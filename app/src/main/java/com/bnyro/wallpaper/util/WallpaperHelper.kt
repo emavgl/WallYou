303bba6d86e90485674fe4b281adf08d9e83b259
Emanuele Viglianisi <emavgl@gmail.com>
2023-12-24 12:06:39 +0100

    test

--- app/src/main/java/com/bnyro/wallpaper/util/WallpaperHelper.kt
+++ app/src/main/java/com/bnyro/wallpaper/util/WallpaperHelper.kt
@@ -60,6 +60,7 @@ object WallpaperHelper {
     private fun getResizedBitmap(bitmap: Bitmap, displayMetrics: DisplayMetrics): Bitmap {
         val screenWidth = displayMetrics.widthPixels
         val screenHeight = displayMetrics.heightPixels
+
         val bitmapWidth = bitmap.width.toFloat()
         val bitmapHeight = bitmap.height.toFloat()
 
@@ -69,32 +70,19 @@ object WallpaperHelper {
         val resizedBitmap = if (screenRatio > bitmapRatio) {
             getResizedBitmap(bitmap, screenWidth, (screenWidth * bitmapRatio).toInt())
         } else {
-            getResizedBitmap(bitmap, (screenHeight / bitmapRatio).toInt(), screenHeight)
+            bitmap
         }
-
-        val bitmapGapX = ((bitmap.width - screenWidth) / 2f).toInt()
-        val bitmapGapY = ((bitmap.height - screenHeight) / 2f).toInt()
-
-        // prevent crashes due to wrong aspect ratio
-        if (bitmapGapX <= 0 || bitmapGapY <= 0) return resizedBitmap
-
-        return runCatching {
-            Bitmap.createBitmap(
-                resizedBitmap,
-                bitmapGapX,
-                bitmapGapY,
-                screenWidth,
-                screenHeight
-            )
-        }.getOrDefault(resizedBitmap)
+        
+        return resizedBitmap
     }
 
     private fun getResizedBitmap(bitmap: Bitmap, newWidth: Int, newHeight: Int): Bitmap {
-        return Bitmap.createScaledBitmap(
-            bitmap,
-            newWidth,
-            newHeight,
-            true
-        )
+        Bitmap scaledBitmap = Bitmap.createBitmap(newWidth, newHeight, Config.ARGB_8888)
+        float ratioX = newWidth / (float) bitmap.getWidth()
+        float ratioY = newHeight / (float) bitmap.getHeight()
+        Canvas canvas = new Canvas(scaledBitmap)
+        canvas.setMatrix(scaleMatrix)
+        canvas.drawBitmap(bitmap, 0, 0, new Paint(Paint.FILTER_BITMAP_FLAG))
+        return scaledBitmap
     }
 }
